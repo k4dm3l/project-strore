cc33b0461c0f8d83a2543ce0ce16e4c537eb65ae
Camilo <c.acevedo1993@gmail.com>
2019-12-30 10:49:21 -0500

    Extends components searchform, form and navbar

--- resources/views/products/update.blade.php
+++ resources/views/products/update.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.main')
+@extends('components.searchform')
+@extends('components.form')
+@extends('components.navbar')
+
 @section('content')
-<h2>Update Product</h2>
+    <div class="container-fluid m-5">
+        <h2>Update Product</h2>
+    </div>
 @endsection
