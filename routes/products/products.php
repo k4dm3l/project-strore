3be001e0767b7440ebd20342eabae36508f44f5a
Camilo <c.acevedo1993@gmail.com>
2019-12-30 10:50:15 -0500

    Commented all routes on products basic router file

--- routes/products/products.php
+++ routes/products/products.php
@@ -1,26 +1,30 @@
 <?php
 
-/** GET PRODUCT */
+/** Normal
+
+// GET PRODUCT
 Route::get('/product/get/{id}', function() {
     return view('products.getone');
 });
 
-/** GET ALL PRODUCTS */
+// GET ALL PRODUCTS
 Route::get('/product/all', function() {
     return view('products.getall');
 });
 
-/** CREATE PRODUCT */
+// CREATE PRODUCT
 Route::get('/product/new', function() {
     return view('products.create');
 });
 
-/** UPDATE PRODUCT */
+// UPDATE PRODUCT
 Route::put('/product/update/{id}', function() {
     return view('products.update');
 });
 
-/** DELETE PRODUCT */
+// DELETE PRODUCT
 Route::delete('/product/delete/{id}', function() {
     return view('products.delete');
 });
+
+ */
