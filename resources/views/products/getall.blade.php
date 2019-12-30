90cbb11300a632b92a38a0ea22aa29cc1f9db4f0
Camilo <c.acevedo1993@gmail.com>
2019-12-30 10:48:19 -0500

    Extends components table and navbar

--- resources/views/products/getall.blade.php
+++ resources/views/products/getall.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.main')
+@extends('components.table')
+@extends('components.navbar')
+
 @section('content')
-<h2>Get All Products</h2>
+    <div class="container-fluid m-5">
+        <h2>Get All Products</h2>
+    </div>
 @endsection
